80a43eeaf529bdb7f73c54eb9322312c2476d80b
priteshshah96 <priteshshahwork@gmail.com>
2025-07-20 21:16:53 -0400

    sigmoid.py updated

--- Logistic Regression/sigmoid.py
+++ Logistic Regression/sigmoid.py
@@ -9,15 +9,26 @@ def sigmoid_derivative(x: float) -> float:
     sig_x = sigmoid(x)
     return sig_x * (1-sig_x)
 
+
+def vectorized_sigmoid(x: list[Union[int, float]]) -> list[float]:
+    sigmoid_list =[]
+    for x_iter in x:
+        x_sig = sigmoid(x_iter)
+        sigmoid_list.append(x_sig)
+    return sigmoid_list
+
+def vectorized_sigmoid_derivation(x: list[Union[int, float]]) -> list[float]:
+    sigmoid_derivative_list = []
+    for x_iter in x:
+        X_derivative = sigmoid_derivative(x_iter)
+        sigmoid_derivative_list.append(X_derivative)
+    return sigmoid_derivative_list
+
 if __name__ == "__main__":
     print("Testing the sigmoid function with hardcoded values of x")
-
+    
     x_values = [0, 1, 0.5, -1, -2.345, 24]
-    for x_iter in x_values:
-        x_sig= float(x_iter)
-        x = sigmoid(x_sig)
-        x_derivative = sigmoid_derivative(x_sig)
-        print(f"sigmoid({x_sig}) = {x}")
-        print(f"sigmoid_derivative({x_sig}) = {x_derivative}")
+    print(f"sigmoid of the list is {vectorized_sigmoid(x_values)}")
+    print(f"sigmoid derivative of the list is {vectorized_sigmoid_derivation(x_values)}")
 
         
